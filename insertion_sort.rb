4b3c0b7079b4a05fc6affa3f25cca16e2ae9d9e8
Mendel Hornbacher <me@mendel.tech>
2017-06-16 08:23:16 -0400

    commented code and used upto over times

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,19 +1,20 @@
 def insertionSort(input_list)
-    alist = input_list.clone
-    (alist.length-1).times do |index|
-        index += 1
-        current_value = alist[index]
-        position = index
+    alist = input_list.clone # copy the array to sort its values. If this is not done it WILL sort in place (modify the list that was passed in)
+    1.upto(alist.size - 1) do |index| # from index 1 to the last do the following
+        current_value = alist[index] # the value of the item we are sorting
+        position = index # its index in the array
 
+         # while we are not at the begining of the array or the previous value is greater then this one (a.k.a it is in the wrong position)
         while position > 0 && alist[position - 1] > current_value do
-            alist[position] = alist[position - 1]
-            position -= 1
+            alist[position] = alist[position - 1] # move the previous item up one
+            position -= 1 # and move the index of our item back
         end
-        alist[position] = current_value
+        alist[position] = current_value # set the value of the final position to the correct value (it will either not change or be a duplicate of alist[position+1])
+        # puts alist.inspect # uncomment this to see how insertion sort works on each iteration
     end
-    return alist
+    return alist # return the new list
 end
 
 alist = [54,26,93,17,77,31,44,55,20]
 list2 = insertionSort(alist)
-puts(list2.inspect, alist.inspect)
+puts  "inital: ", alist.inspect, "Sorted: ", list2.inspect
